2c419a343cfd77ff1f836cb3fce09bc6df0b4f6f
阎垚月 <yanyaoyue@hualala.com>
2017-11-14 11:26:33 +0800

    初始化项目，适配手机端

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <router-link to="/hello">跳转第一个页面</router-link> 
+    <router-link to="/">跳转第二个页面</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'app',
+  computed: mapState({
+    // 全局接口请求数据的loading状态
+    requestFail: state => state.global.requestFail,
+    // 监听接口请求数据的报错状态
+    requestLoading: state => state.global.requestLoading
+  }),
+  watch: {
+    requestFail (data) {
+      console.log('请求失败了', data)
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/less" lang="less">
+  @import './styles/base-750.less';
+  @import './styles/reset.less';
+  @import './styles/border-1px.less';
+body {
+  margin: 0;
+  padding: 0;
+  min-height: 100%;
+  width: 100%;
+  a {
+    .font-size(36);
+    .margin(16);
+  }
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
